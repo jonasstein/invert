6d50c57a3a98ac11f32367a192fd0f94ed931b74
Jonas Stein <news@jonasstein.de>
2010-08-25 19:50:03 +0200

    update

--- invert.f95
+++ invert.f95
@@ -2,6 +2,7 @@ program invert
   implicit none
 
   real, allocatable, dimension(:,:) :: A         !  n x n Matrix
+  real, allocatable, dimension(:,:) :: identity  !  n x n Matrix
 
   integer :: n, m   !Dimension
   integer :: dim    !Dimension
@@ -13,57 +14,86 @@ program invert
 
   read *, dim
   allocate(A(dim, dim))
+  allocate(identity(dim, dim))
 
   do n = 1, dim, 1 ! n lauft von 1 bis 4 mit Schrittweite 1
      do m = 1, dim, 1
         Wert = int(rand() * 10 + 0.5)  ! rand() = Zufallszahlengenerator (0..1)
-900     format(2(I3,1x), 1x, F3.1)  ! https://srv.rz.uni-bayreuth.de/lehre/fortran90/vorlesung/V05/V05.html
+900     format(2(I3,1x), 1x, F4.1)  ! https://srv.rz.uni-bayreuth.de/lehre/fortran90/vorlesung/V05/V05.html
         write(*, 900) n, m, Wert
         A(n, m)=Wert
      end do
   end do
 
-write (*,*) identity(3)
+
+  write(*,*) "===================================="
 
 
-end program invert
-
-
-function  identity(dimmi)
-  implicit none
-  integer :: n, m 
-  integer, intend ( in ) :: dimmi
-  real, allocatable, dimension(:,:) :: identity
-  allocate(identity(dimmi, dimmi))
-
-  do n = 1, dimmi, 1 ! n lauft von 1 bis 4 mit Schrittweite 1
-     do m = 1, dimmi, 1
+  do n = 1, dim, 1
+     do m = 1, dim, 1
         if (n == m) then 
            identity(n, m) = 1 
         else 
            identity(n, m) = 0 
         endif
+        write(*, 900) n, m, identity(n,m)
      end do
   end do
-  write (*,*) identity
-  return
-end function identity
-
-     ! function rowmul(A, row, factor) 
 
 
+  n = 1 ! n-te Zeile mit 5 Multiplizieren
+  do m = 1, dim, 1
+     A(n,m)= A(n,m) * A(2,1)
+  end do
 
-     function showmatrix_serial(A,dim)
-       integer n, m, dim
+ 
+ n = 2 
+ do m = 1, dim, 1
+     A(n,m)= A(n,m) * A(1,1)
+  end do
 
-       do n = 1, dim, 1
-          do m = 1, dim, 1
-             write(*,*) 'n =', n
-          end do
-       end do
 
-       return  
-     end function showmatrix_serial
+ do m = 1, dim, 1
+     A(1,m)= A(1,m) - A(2,m)
+  end do
+
+
+
+
+
+
+
+!Ausgeben
+  do n = 1, dim, 1 
+     do m = 1, dim, 1
+        Wert = A(n,m)
+        write(*, 900) n, m, Wert
+     end do
+  end do
+
+
+end program invert
+
+
+
+
+
+
+
+
+
+
+function showmatrix_serial(A,dim)
+  integer n, m, dim
+
+  do n = 1, dim, 1
+     do m = 1, dim, 1
+        write(*,*) 'n =', n
+     end do
+  end do
+
+  return  
+end function showmatrix_serial
 
 
 
